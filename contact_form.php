350eb32ebfc29e2648b6b88d6f8eb81a14ae32ed
Michael Kern <kernm@in.tum.de>
2014-02-06 19:23:41 +0100

    added phpmailer to project. Now request can be sendet to target

--- contact_form.php
+++ contact_form.php
@@ -1,14 +1,17 @@
-
+<div id='contact_div' align='center'>
 <?php
 	
 	// contact items
-	$firstname = (isset($_POST["firstname"])) ? $_POST["firstname"] : "";
-	$name = (isset($_POST["lastname"])) ? $_POST["lastname"] : "";
-	$address = (isset($_POST["address"])) ? $_POST["address"] : "";
-	$town = (isset($_POST["town"])) ? $_POST["town"] : "";
-	$mail = (isset($_POST["e-mail"])) ? $_POST["e-mail"] : "";
-	$phone = (isset($_POST["phone"])) ? $_POST["phone"] : "";
-	$message = (isset($_POST["message"])) ? $_POST["message"] : "";
+	
+	// htmlspecialchars: convert html special chars to html code
+	// strip_tags: remove xhtml tags
+	$firstname = (isset($_POST["firstname"])) ? htmlspecialchars(strip_tags($_POST["firstname"], ENT_QUOTES)) : "";
+	$name = (isset($_POST["lastname"])) ? htmlspecialchars(strip_tags($_POST["lastname"], ENT_QUOTES)) : "";
+	$address = (isset($_POST["address"])) ? htmlspecialchars(strip_tags($_POST["address"], ENT_QUOTES)) : "";
+	$town = (isset($_POST["town"])) ? htmlspecialchars(strip_tags($_POST["town"], ENT_QUOTES)) : "";
+	$mail = (isset($_POST["e-mail"])) ? htmlspecialchars(strip_tags($_POST["e-mail"], ENT_QUOTES)) : "";
+	$phone = (isset($_POST["phone"])) ? htmlspecialchars(strip_tags($_POST["phone"], ENT_QUOTES)) : "";
+	$message = (isset($_POST["message"])) ? htmlspecialchars(strip_tags($_POST["message"], ENT_QUOTES)) : "";
 	
 	// fake items
 	$fakeMail = (isset($_POST["email"])) ? $_POST["email"] : "";
@@ -52,8 +55,8 @@
 	{
 		global $formItems, $fakes, $fakeItems;
 		
-		//$missingValue = false;
 		
+		// search for fakes!
 		foreach($fakeItems as $key => $value)
 		{
 			if (!empty($value))
@@ -63,6 +66,10 @@
 			}
 		}
 		
+		// check false email?
+		// with filter_var($mail, FILTER_VALIDATE_MAIL | FILTER_SANITIZE_MAIL);
+		
+		// search for missing entries
 		foreach($formItems as $key => $value)
 		{
 			if (in_array($key, $fakes))
@@ -71,15 +78,13 @@
 			if (empty($value))
 			{
 				displayContactForm();
-				echo "Error. Values are missing";
+				echo "<strong><p>Sie haben vergessen, alle Felder auszufüllen.<br> Bitte füllen Sie alle rot markierten Felder aus!</p></strong>";
 				return;
 			}
 			
 		}
 		
-		
-		echo "Dankeschön.";
-		
+		sendMail();	
 	}
 
 	function displayContactForm()
@@ -89,7 +94,6 @@
 		$submitDone = isset($_POST["submit"]);
 		
 		?>
-		<div id='contact_div' align='center'>
 			<strong><p>Nehmen Sie mit uns hier schnell und einfach Kontakt auf.</p>
 			<p>Wir freuen uns über jede Anfrage!</p></strong><br>
 			<form action='?page=2' method='post'>
@@ -111,9 +115,13 @@
 			echo ($submitDone && !$isFake && empty($value)) ? "<td class='missing'>" : "<td>";
 			
 			if ($key == "message")
-				echo "<textarea id='$key' name='$key' rows='10' cols='25' value='$value'></textarea>";
+			{
+				echo "<textarea id='$key' name='$key' rows='10' cols='25' value=''>$value</textarea>";
+			}
 			else
+			{
 				echo "<input type='text' id='$key' name='$key' size='30' maxlength='40' value='$value'>";
+			}
 			
 			echo "</td></tr>";
 		}
@@ -123,8 +131,64 @@
 		<tr><td>&nbsp;</td><td><input type='submit' name='submit' value='Absenden'></td></tr>
 				</table>
 			</form>
-		</div>
 		
 		<?php
 	}
+	
+	function sendMail()
+	{
+		global $firstname, $name, $address, $town, $mail, $phone, $message;
+		
+		require 'import/class.phpmailer.php';
+		
+		$requester = $firstname." ".$name;
+		
+		$mailMessage = "Sehr geehrter Bezirkstags,<br>
+						eine neue Anfrage ist eingetroffen!<br><br>
+						<strong>$requester</strong> schreibt:<br>
+
+						=========================================<br>
+						$message<br>
+						=========================================<br>
+
+						<strong>Kontaktdaten:</strong><br><ul>
+						<li>Adresse: $address, $town</li>
+						<li>Telefon: $phone</li>
+						<li>E-Mail: $mail</li>
+						</ul>";
+
+		$mailSubject = "[Bezirkstagsfraktion.de] Neue Anfrage von $requester";
+		
+		// create new mail service
+		$mailer = new PHPMailer();
+		// set mail service settings
+		
+		// smtp settings
+		$mailer->IsSMTP();
+		$mailer->SMTPDebug = 1;
+		// sender
+		$mailer->From = "michi.kern@kabelmail.de";
+		$mailer->FromName = "Bezirkstagsfraktion Bayern";
+		// smtp security settings SMTP with authentication
+		$mailer->SMTPAuth = true;
+		$mailer->Host = "smtp.kabelmail.de";
+		$mailer->Port = 25;
+		// authentication data
+		$mailer->Username = "michi.kern@kabelmail.de";
+		$mailer->Password = "M12345678";
+		// receiver
+		$mailer->AddAddress("kernm@in.tum.de");
+		// mail content
+		$mailer->Subject = $mailSubject;
+		$mailer->Body = $mailMessage;
+		$mailer->IsHTML(true);
+		
+		if ($mailer->Send())
+			echo "<strong><p>Ihre Anfrage wurde erfolgreich gesendet. Vielen Dank.</p></strong>";
+		else
+			echo $mailer->ErrorInfo."<br><strong><p>Leider ist ein unerwarteter Fehler beim Senden der Anfrage eingetreten!<br>Bitte probieren Sie es später nochmal. Wir bitten um Ihr Verständnis.</p></strong>";
+		
+	}
 ?>
+
+</div>
